c2dc4b6341ea90f0e77e7d8fd7585bfde9fb98ba
Abhitipu <abhinandan0316@gmail.com>
2022-09-16 01:31:28 +0530

    Add plots

--- analyze.py
+++ analyze.py
@@ -4,11 +4,33 @@ import gym
 import matplotlib.pyplot as plt
 
 attributes = ["gravity", "masscart", "masspole", "length", "force_mag"]
-WINDOW_SIZE = 20
+WINDOW_SIZE = 2000
 N_TRAIN_EPISODES = 100000
 
 X = np.arange(0, N_TRAIN_EPISODES, WINDOW_SIZE)
 environment = gym.make("CartPole-v1", render_mode="rgb_array")
+
+def save_train(attr, train_rewards, new_values, X):
+    plt.cla()
+    plt.title(f"Rewards from training with tweaks in {attr}")
+    plt.xlabel("Episode")
+    plt.ylabel("Reward")
+    _ = plt.plot(X, np.array(train_rewards).T)
+    _ = plt.legend(new_values)
+    plt.savefig(f"graphs/train_rewards_{attr}.jpg")
+    plt.close()
+    plt.show()
+
+def save_test(attr, new_values, mu):
+    plt.cla()
+    plt.title(f"Mean rewards from testing with tweaks in {attr}")
+    plt.xlabel(f"New values of {attr}")
+    plt.ylabel("Mean reward")
+    _ = plt.bar(new_values, mu)
+    plt.savefig(f"graphs/test_rewards_{attr}.jpg")
+    plt.show()
+    plt.close()
+
 
 for attr in attributes:
     with open(f"rewards/train_rewards_{attr}.pkl", "rb") as f:
@@ -25,16 +47,8 @@ for attr in attributes:
         my_rewards = np.array(line.get_ydata())
         train_rewards.append(np.convolve(my_rewards, np.ones(WINDOW_SIZE))[X] / WINDOW_SIZE)
         
-    plt.cla()
-    plt.title(f"Rewards from training with tweaks in {attr}")
-    plt.xlabel("Episode")
-    plt.ylabel("Reward")
-    _ = plt.plot(X, np.array(train_rewards).T)
-    _ = plt.legend(new_values)
-    plt.savefig(f"graphs/train_rewards_{attr}.jpg")
-    # plt.show()
+    save_train(attr, train_rewards, new_values, X)   
 
-    plt.cla()
     with open(f"rewards/test_rewards_{attr}.pkl", "rb") as f:
         axes = pickle.load(f)
     
@@ -42,14 +56,9 @@ for attr in attributes:
     for line in axes.lines:
         my_rewards = np.array(line.get_ydata())
         test_rewards.append(my_rewards)
-	
     test_rewards = np.array(test_rewards)
-    print(test_rewards.shape)
     mu = np.mean(test_rewards, axis=1)
-    plt.title(f"Mean rewards from testing with tweaks in {attr}")
-    plt.xlabel(f"New values of {attr}")
-    plt.ylabel("Mean reward")
-    _ = plt.bar(new_values, mu)
-    plt.savefig(f"graphs/test_rewards_{attr}.jpg")
+
+    save_test(attr, new_values, mu)
     # plt.show()
 
