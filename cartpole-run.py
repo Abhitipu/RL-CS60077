a2935da376455b2e562be4da1f80791471f51f04
AdityaAnantwar <53852678+AdityaAnantwar@users.noreply.github.com>
2022-10-23 17:11:02 +0530

    phase 2 script
    add in cleanrl folder

---
+++ cartpole-run.py
@@ -0,0 +1,26 @@
+import gym
+import numpy as np
+import os
+
+env = gym.make('CartPole-v1')
+
+attributes = ["gravity", "masscart", "masspole", "length", "force_mag"]
+
+for attr in attributes:
+    # obtain original value
+    orig_value = getattr(env, attr)
+    
+    all_test_rewards = []
+    
+    new_values = np.linspace(0, 2 * orig_value, 5)
+    new_values = np.round(new_values, decimals = 2)
+
+    with open('cartpole-values.txt', 'a') as f:
+        for value in new_values:
+            f.write(attr + ' ' + str(value) + '\n')
+
+with open('cartpole-values.txt', 'r') as f:
+    for line in f:
+        if line != "":
+            attr, val = line.split(' ')
+            os.system(f'python cleanrl/ppo.py --env-id CartPole-v1 --attribute {attr} --value {val}')
