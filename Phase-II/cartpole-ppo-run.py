78c405c3d383be0744ee1426e35dabeaa51b5da8
Abhinandan De <55925763+Abhitipu@users.noreply.github.com>
2022-11-11 15:32:28 +0530

    Update cartpole-ppo-run.py

--- Phase-II/cartpole-ppo-run.py
+++ Phase-II/cartpole-ppo-run.py
@@ -1,9 +1,17 @@
 import os
 
 if __name__ == "__main__":
-    # attributes = ["gravity", "masscart", "masspole", "length", "force_mag"]
-    attributes = ["force_mag"]
-    tt = 51200
+    attributes = ["gravity", "masscart", "masspole", "length", "force_mag"]
+    # attributes = ["masscart"]
+    tt = int(51200 * 4)
+    
+    factors = {
+        "force_mag": 3,
+        "length": 10,
+        "masspole": 3,
+        "masscart": 3,
+        "gravity": 2,
+    }
     
     for attr in attributes:
-        os.system(f'poetry run python ppo_trsnsfer.py --env-id CartPole-v1 --attribute {attr} --total-timesteps {tt} > {attr}.txt')
+        os.system(f'poetry run python ppo_transfer.py --env-id CartPole-v1 --attribute {attr} --total-timesteps {tt} --factor {factors[attr]} --num-changes 5 > {attr}.txt')
